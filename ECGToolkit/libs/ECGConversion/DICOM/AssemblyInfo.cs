84fb92c69f3edc7e1d42ee3a75a70b1e91322003
Maarten J.B. van Ettinger <m.vanettinger@erasmusmc.nl>
2019-05-20 17:00:12 +0200

    Major commit after migrating to git.
    Added functionality in the past years:
    - Improved support for BufferedStream for CSV, PDF
    - Improved DICOM-ECG support
    - Added DICOM with Encapsulated PDF support
    - 15-lead support for ECGViewer, ECGDraw and PDF plug-in
    - Solved some bugs in support for aECG and ISHNE

--- ECGToolkit/libs/ECGConversion/DICOM/AssemblyInfo.cs
+++ ECGToolkit/libs/ECGConversion/DICOM/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("van Ettinger Information Technology")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Copyright 2012-2014, van Ettinger Information Technology, Lopik, The Netherlands\nCopyright 2008-2010, Thoraxcentrum, Erasmus MC, Rotterdam, The Netherlands")]
+[assembly: AssemblyCopyright("Copyright 2019, Thoraxcentrum, Erasmus MC, Rotterdam, The Netherlands\nCopyright 2012-2014, van Ettinger Information Technology, Lopik, The Netherlands\nCopyright 2008-2012, Thoraxcentrum, Erasmus MC, Rotterdam, The Netherlands")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]		
 
